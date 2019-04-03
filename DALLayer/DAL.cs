336aff74b7c44ed14bd4141ff88b8952fe1d6c93
NafeeshGridInfocom <nafeesh.ahmed@gridinfocom.com>
2019-04-03 10:39:25 +0530

    Add project files.

---
+++ DALLayer/DAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using BELayer;
+
+namespace DALLayer
+{
+    public class DAL
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlConnection getconn()
+        {
+            string str = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            return con;
+        }
+        public int Insert(student stu)
+        {
+            int flag = 0;
+            using (con = getconn())
+            {
+                using (cmd = new SqlCommand("insert into Deprtment(id,name,domain_area,no_of_emp,location)values(@id,@name,@domain_area,@no_of_emp,@location)", con)) ;
+                cmd.Parameters.AddWithValue("@id", stu.id);
+                cmd.Parameters.AddWithValue("@name", stu.name);
+                cmd.Parameters.AddWithValue("@domain_area", stu.domain_area);
+                cmd.Parameters.AddWithValue("@no_of_emp", stu.no_of_emp);
+                cmd.Parameters.AddWithValue("@location", stu.location);
+                con.Open();
+                flag = cmd.ExecuteNonQuery();
+                con.Close();
+                Console.Write("Data Inserted");
+            }
+            return flag;
+        }
+
+    }
+
+}
+
+   
